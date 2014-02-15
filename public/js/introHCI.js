342d5c57d559681f3f08aed8971a417cf2adc0dc
Arushi Jain <arushij@stanford.edu>
2014-02-15 01:01:50 +0000

    lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -17,16 +17,34 @@ function initializePage() {
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
+
+
+
+
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
-
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber, callBack);
+	console.log(this.url);
+}
+
+function callBack(result){
+	console.log(result);
+
+	var html = '<a href = "" class="thambnail">' + 
+			'<img src=" ' + result['image'] + '"class="detailsImage">' +
+			'<h1>' + result['title'] + '</h1>' +
+			'<p>' + result['summary'] + '</p>' 
+	;
+
+	$("#project" + result['id']).html(html);
+
 }
 
 /*
@@ -35,4 +53,10 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get('/palette/', colorCallback);
+}
+
+function colorCallback(result){
+	
+	
 }
